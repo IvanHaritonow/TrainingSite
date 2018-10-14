a87429ef3ab72f963b652ac4c654733beed55419
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-10-14 23:55:36 +0300

    Update b0.php

--- b0.php
+++ b0.php
@@ -1,11 +1,12 @@
 
 <meta charset="utf-8"> 
 <?php
-$urok="Урок 22";
+$urok="Новая заявка";
 error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
 // создание переменных из полей формы		
 if (isset($_POST['name1']))			{$name1			= $_POST['name1'];		if ($name1 == '')	{unset($name1);}}
 if (isset($_POST['email1']))		{$email1		= $_POST['email1'];		if ($email1 == '')	{unset($email1);}}
+if (isset($_POST['tel']))			{$sab			= $_POST['tel1'];		if ($tel1 == '')		{unset($tel1);}}
 if (isset($_POST['text']))			{$text			= $_POST['text'];		if ($text == '')	{unset($text);}}
 if (isset($_POST['sab']))			{$sab			= $_POST['sab'];		if ($sab == '')		{unset($sab);}}
 //стирание треугольных скобок из полей формы
@@ -17,6 +18,10 @@ if (isset($email1) ) {
 $email1=stripslashes($email1);
 $email1=htmlspecialchars($email1);
 }
+if (isset($tel1) ) {
+$tel1=stripslashes($tel1);
+$tel1=htmlspecialchars($tel1);
+}
 if (isset($text) ) {
 $text=stripslashes($text);
 $text=htmlspecialchars($text);
@@ -24,7 +29,7 @@ $text=htmlspecialchars($text);
 // адрес почты куда придет письмо
 $address="haritonow1@list.ru";
 // текст письма 
-$note_text="Тема : $urok \r\nИмя : $name1 \r\n Email : $email1 \r\n Дополнительная информация : $text";
+$note_text="Тема : $urok \r\nИмя : $name1 \r\nEmail : $email1 \r\nТелефон : $tel1 \r\nФорма обучения : $text";
 
 if (isset($name1)  &&  isset ($sab) ) {
 mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
